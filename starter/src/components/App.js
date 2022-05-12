f25db4abc436e218e72c3dd892c78e2f0d03ac60
DurandHippolyte <hippolyte410@gmail.com>
2022-05-12 17:00:06 +0200

    shop project

---
+++ starter/src/components/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
+
+function App() {
+
+  
+
+   return (
+      <div>
+         <h2>Redux Toolkit</h2>
+         <Navbar />
+         <CartContainer />
+      </div>
+   );
+}
+export default App;
---
+++ starter/src/components/CartContainer.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { clearCart } from "../features/cartSlice";
+import CartItem from "./CartItem";
+
+const CartContainer = () => {
+
+   const dispatch = useDispatch()
+
+   // contient un tableau d'objet
+   const cartItems = useSelector((store) => store.cartSliceStore.cartItems);
+
+   const { amount, total } = useSelector((state) => state.cartSliceStore);
+
+   if (amount < 1) {
+      return (
+         <section className="cart">
+            <header>
+               <h2>your bag</h2>
+               <h4 className="empty-cart"> is currently empty</h4>
+            </header>
+         </section>
+      );
+   } else {
+      return (
+         <section className="cart">
+            <header>
+               <h2>your bag</h2>
+            </header>
+            <div>
+               {cartItems.map((item) => {
+                  return (
+                     <CartItem
+                        key={item.id}
+                        title={item.title}
+                        price={item.price}
+                        img={item.img}
+                        amount={item.amount}
+                     />
+                  );
+               })}
+            </div>
+            <footer>
+               <hr />
+               <div className="cart-total">
+                  <h4>
+                     total <span> ${total} </span>
+                  </h4>
+               </div>
+               <button className="btn clear-btn"
+               onClick={()=>{dispatch(clearCart())}}>clear cart</button>
+            </footer>
+         </section>
+      );
+   }
+};
+
+export default CartContainer;
